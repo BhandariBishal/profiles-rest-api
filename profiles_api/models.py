9d0694106fadba9e632e33833d227dc4ffe28287
Bishal <bhandaribishal777@gmail.com>
2021-07-24 13:40:28 +0545

    added profiles viewset

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -10,7 +10,7 @@ class UserProfileManager(BaseUserManager):
         """create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
-        email = self.normalize_email('email')
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
